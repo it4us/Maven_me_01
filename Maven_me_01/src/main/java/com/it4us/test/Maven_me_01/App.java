075d90ab672b1854b7903cce9f2b9a4561a3cc1d
it4us2 <post4info@arcor.de>
2019-01-31 18:24:42 +0100

    Eclipse to GitHub

---
+++ Maven_me_01/src/main/java/com/it4us/test/Maven_me_01/App.java
@@ -0,0 +1,13 @@
+package com.it4us.test.Maven_me_01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
